bb7e0ef955f33cbc11ee41e06aa5f58ca8c1756a
Lorenzo <lorenzo.delsignore94@gmail.com>
2023-03-22 22:27:11 +0100

    Add attention mechanism

--- model.py
+++ model.py
@@ -24,3 +24,47 @@ class PatchEmbed(nn.Module):
         x = x.flatten(2) # (n_samples, embed_dim, n_patches)
         x = x.transpose(1, 2) # (n_samples, n_patches, embed_dim)
         return x
+
+
+class Attention(nn.Module):
+    """Attention mechanism."""
+    def __init__(self, dim, n_heads=12, qkv_bias=True, attn_p=0., proj_p=0.):
+        super().__init__()
+        self.dim = dim
+        self.n_heads = n_heads
+        self.head_dim = dim // n_heads # same dimension as the input when concateneted
+        self.scale = self.head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim*3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_p)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_p)
+    
+    def forward(self, x):
+        """Run forward pass
+        Input
+        -----
+        x : torch.Tensor
+            Shape (n_samples, n_patches + 1, dim)
+        """
+        n_samples, n_tokens, dim = x.shape
+
+        if dim != self.dim:
+            raise ValueError
+        
+        qkv = self.qkv(x) # (n_samples, n_patches + 1, 3 * dim)
+        qkv = qkv.reshape(n_samples, n_tokens, 3, self.n_heads, self.head_dim) # (n_samples, n_patches + 1, 3, n_heads, head_dim)
+        qkv = qkv.permute(2, 0, 3, 1, 4) # (3, n_samples, n_heads, n_patches + 1, head_dim)
+        q, k, v = qkv[0], qkv[1], qkv[2]
+        k_t = k.transpose(-2, -1) # (n_samples, n_heads, head_dim, n_patches + 1)
+        dp = (q @ k_t) * self.scale # (n_samples, n_heads, n_patches + 1, n_patches + 1)
+        attn = dp.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        weighted_avg = attn @ v # (n_samples, n_heads, n_patches + 1, head_dim)
+        weighted_avg = weighted_avg.transpose(1,2) # (n_samples, n_patches + 1, n_heads , head_dim)
+        weighted_avg = weighted_avg.flatten(2) # (n_samples, n_patches + 1, dim)
+        x = self.proj(weighted_avg) # (n_samples, n_patches + 1, dim)
+        x = self.proj_drop(x)
+
+        return x
