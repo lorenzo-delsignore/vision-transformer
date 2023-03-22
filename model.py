9ef156e32b9b876fb81d5f23d374837af787a3df
Lorenzo <lorenzo.delsignore94@gmail.com>
2023-03-22 22:23:43 +0100

    Add PatchEmbed class

---
+++ model.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+
+
+class PatchEmbed(nn.Module):
+    """Split image into patches and embed them"""
+    def __init__(self, img_size, patch_size, in_chans=3, embed_dim=768):
+        super().__init__()
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.n_patches = (img_size // patch_size) ** 2
+        self.in_chans = in_chans
+        self.embed_dim = embed_dim
+        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
+
+    def forward(self, x):
+        """Run forward pass
+        Input
+        -----
+        x : torch.Tensor
+            Shape (n_samples, in_chans, img_size, img_size)
+        """
+        x = self.proj(x) # (n_samples, embed_dim, n_patches ** 0.5, n_patches ** 0.5)
+        x = x.flatten(2) # (n_samples, embed_dim, n_patches)
+        x = x.transpose(1, 2) # (n_samples, n_patches, embed_dim)
+        return x
